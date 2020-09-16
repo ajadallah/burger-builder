d153a3569bc9420df4aee2fce3f3bd9725d55283
Aref <arefajadallah@gmail.com>
2020-09-16 19:42:11 -0400

    Build build controls

---
+++ src/components/burger/buildControls/BuildControls.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function BuildControls(props) {
+  return (
+    <div>
+      Build Controls
+    </div>
+  );
+}
---
+++ src/components/burger/buildControls/buildControl/BuildControl.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import PropTypes from 'prop-types';
+import classes from './BuildControls.module.css';
+
+export default function BuildControl(props) {
+  const { label } = props;
+
+  return (
+    <div className={classes.BuildControl}>
+      <div className={classes.Label}>{label}</div>
+      <button className={classes.Less} type="button">Less</button>
+      <button className={classes.More} type="button">More</button>
+    </div>
+  );
+}
+BuildControl.propTypes = {
+  label: PropTypes.objectOf.isRequired,
+};
