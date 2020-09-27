81d593bafda8cef17e2c41750ba4b1e31321ba47
Aref <arefajadallah@gmail.com>
2020-09-27 19:50:03 -0400

    Add Logo

---
+++ src/components/logo/Logo.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import burgerLogo from '../../assets/images/burger-logo.png';
+import classes from './Logo.module.css';
+
+function Logo(props) {
+  return (
+    <div className={classes.Logo}>
+      <img src={burgerLogo} alt="MyBurger" />
+    </div>
+  );
+}
+
+Logo.propTypes = {
+
+};
+
+export default Logo;
--- src/components/navigation/toolbar/Toolbar.js
+++ src/components/navigation/toolbar/Toolbar.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import classes from './Toolbar.module.css';
+import Logo from '../../logo/Logo';
 
 const Toolbar = (props) => (
   <header className={classes.Toolbar}>
     <div>MENU</div>
-    <div>LOGO</div>
+    <Logo />
     <nav>...</nav>
   </header>
 );
