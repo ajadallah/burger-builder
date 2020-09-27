8838913e7e46df973bd4aa7017036eab5507f10e
Aref <arefajadallah@gmail.com>
2020-09-27 19:13:43 -0400

    Add Toolbar to Layout component

--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -4,10 +4,11 @@ import PropTypes from 'prop-types';
 
 import Auxiliary from '../../hoc/Auxiliary';
 import classes from './Layout.module.css';
+import Toolbar from '../navigation/toolbar/Toolbar';
 
 const layout = (props) => (
   <Auxiliary>
-    <div>Toolbar, SideDrawer, Backdrop</div>
+    <Toolbar />
     <main className={classes.Content}>{props.children}</main>
   </Auxiliary>
 
---
+++ src/components/navigation/toolbar/Toolbar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import classes from './Toolbar.module.css';
+
+const Toolbar = (props) => (
+  <header className={classes.Toolbar}>
+    <div>MENU</div>
+    <div>LOGO</div>
+    <nav>...</nav>
+  </header>
+);
+
+Toolbar.propTypes = {
+
+};
+
+export default Toolbar;
