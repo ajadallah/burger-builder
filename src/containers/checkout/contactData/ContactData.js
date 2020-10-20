80bdcda6adb1e1d0429f3d862cc243c575c232c7
Aref <arefajadallah@gmail.com>
2020-10-20 19:48:10 -0400

    Bugfix for dropdown

--- src/containers/checkout/contactData/ContactData.js
+++ src/containers/checkout/contactData/ContactData.js
@@ -84,7 +84,8 @@ export default class ContactData extends Component {
             { value: 'cheapest', displayValue: 'Cheapest' }
           ]
         },
-        value: '',
+        value: 'fastest',
+        validation: {},
         valid: true
       },
     },
